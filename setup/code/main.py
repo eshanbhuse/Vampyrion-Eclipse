646accc713b0300548b6fbcf50da2056dbf2e1cd
Eshan Bhuse <eshan.bhuse@spit.ac.in>
2025-10-12 18:16:55 +0530

    added shooting animation and logic

--- setup/code/main.py
+++ setup/code/main.py
@@ -11,12 +11,34 @@ class Game:
         pygame.display.set_caption("Vampyrion Eclipse")
         self.clock = pygame.time.Clock()
         self.running = True
+        self.load_images()
 
         self.all_sprites = AllSprites()
 
         self.collision_sprites = pygame.sprite.Group()
+        self.bullet_sprites = pygame.sprite.Group()
 
         self.setup()
+
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.gun_cooldown = 100
+
+    def load_images(self):
+        self.bullet_surf = pygame.image.load(join('images','gun','bullet.png')).convert_alpha()
+        
+    def input(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            pos = self.gun.rect.center + self.gun.player_direction * 50
+            Bullet(self.bullet_surf, pos, self.gun.player_direction, (self.all_sprites, self.bullet_sprites))
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+    def gun_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time >= self.gun_cooldown:
+                self.can_shoot = True
 
     def setup(self):
         map = load_pygame(join('data', 'maps', 'world.tmx'))
@@ -32,6 +54,7 @@ class Game:
         for obj in map.get_layer_by_name('Entities'):
             if obj.name == 'Player':
                 self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+                self.gun = Gun(self.player, self.all_sprites)
 
     def run(self):
         while self.running:
@@ -39,6 +62,10 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+            
+            self.gun_timer()
+
+            self.input()
 
             self.all_sprites.update(dt)
 
